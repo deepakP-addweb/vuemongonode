b7f7df8cf70bc3c4bf6693004b506ba13d005a49
deepakP-addweb <deepak.p@addwebsolution.in>
2023-11-24 11:03:20 +0530

    vuejs frontend

---
+++ frontend/src/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wel">
+     
+  </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+
+<style scoped>
+
+.wel{
+    color: rgba(54, 50, 50, 0.836);
+    padding: 2rem;
+    margin-top: 7rem;
+    text-align: center;
+}
+</style>
